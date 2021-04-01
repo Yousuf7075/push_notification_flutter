6be2a73503d56b48b7e23a06bab62cd9c40d5f5b
Yousuf <aarajyousuf.qtec@gmail.com>
2021-04-01 19:33:26 +0600

    working on ... push notfication

--- lib/main.dart
+++ lib/main.dart
@@ -82,23 +82,43 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
+    super.initState();
+
+    /*FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage message) {
+      if (message != null) {
+        Navigator.pushNamed(context, '/message',
+            arguments: MessageArguments(message, true));
+      }
+    });*/
 
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Got a message whilst in the foreground!');
-      print('Message data: ${message.data}');
-
-      if (message.notification != null) {
-        print('Message also contained a notification: ${message.notification}');
+      RemoteNotification notification = message.notification;
+      AndroidNotification android = message.notification?.android;
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channel.description,
+                // TODO add a proper drawable resource to android, for now using
+                //      one that already exists in example app.
+                icon: 'launch_background',
+              ),
+            ));
       }
     });
 
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
       print('A new onMessageOpenedApp event was published!');
-      print("onMessageOpenedApp: $message");
-      /*Navigator.pushNamed(context, '/message',
-          arguments: MessageArguments(message, true));*/
+     // Navigator.pushNamed(context, '/message',
+          //arguments: MessageArguments(message, true));
     });
-
   }
 
   void _incrementCounter() {
